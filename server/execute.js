4af7d15b7acb25a4adf51aad166350a8275fb500
Amrinder Grewal <agrewal@pd-agrewal.local>
2017-07-13 14:46:18 -0400

    hours submitted with script

--- server/execute.js
+++ server/execute.js
@@ -6,11 +6,14 @@ let CreateDriver = require('./CreateDriver'),
   Promise = require('bluebird'),
   WaitTimeOut = require('./wait-time-out');
 
+const days = ['DOWmonday', 'DOWtuesday', 'DOWwednesday', 'DOWthursday', 'DOWfriday'];
+
 class Execute {
 
   constructor(url, browser) {
     this.url = url;
     this.browser = browser;
+    this.errors = [];
   }
 
   authenticateUser(username, password) {
@@ -35,15 +38,41 @@ class Execute {
 
   validateTimeEntryAndSubmit() {
     return new Promise((resolve, reject) => {
-      let days = ['DOWmonday', 'DOWtuesday', 'DOWwednesday', 'DOWthursday', 'DOWfriday'];
       let promise = _.map(days, (day) => {
         this.click(Execute.dayBinder(day, null))
-          .then(() => this.getElementText(Execute.dayBinder(day, 'span')))
-          .then((savedHours) => {
-            if (savedHours === '8.00') {
-              // todo
-              return Promise.resolve();
+          .then(() => this.checkStatus(day))
+          .then((status) => {
+            if (status[2] === 'Posted to E1' || status[2] === 'Submitted and Approved' ||
+                status[2] === 'Submitted to E1') {
+              throw `Hours for ${day} has been already entered.`;
             }
+            if (status[2] === 'Saved Only') {
+              status = _.filter(status, (s) => s !== '--');
+              status.pop();
+              let promise = _.map(status, (s) => {
+                let hours = parseInt(s);
+                if (hours > 8) {
+                  throw `You can not enter more than 8 hours. User tried: ${status[0]}`;
+                } else if (hours < 0) {
+                  throw `You can not enter Zero hours. User tried: ${status[0]}`
+                } else {
+                  //todo
+                  this.click(WebElements.SUBMIT_SAVED_HOURS)
+                    .then(() => this.handleAlert())
+                    .then(this.wait(500))
+                    .then(() => this.checkStatus(day))
+                    .then((status) => {
+                      if (status[1] !== 'Posted to E1' || status[1] !== 'Submitted and Approved') {
+                        //throw `Hours not submitted for ${day}`;
+                      }
+                      return Promise.resolve();
+                    });
+                }
+              });
+              return Promise.all(promise);
+            }
+            console.log(status);
+            return Promise.resolve();
           });
       });
       Promise.all(promise).then(resolve, reject)
@@ -52,16 +81,19 @@ class Execute {
 
   submitHours(timeSheet) {
     return new Promise((resolve, reject) => {
-      let promise = _.map(timeSheet, (day) => {
-        console.log(`Entering data for ${day.projectName}`);
-        if (day.totalHours > 0) {
-          return this.sendData(WebElements.SEARCH_WORK_ORDER, day.orderNumber)
-            .then(() => this.click(WebElements.SEARCH_BUTTON))
-            .then(() => this.click(WebElements.TYPE_HEAD))
-            .then(() => this.sendData(WebElements.START_DATE, day.startTime))
-            .then(() => this.sendData(WebElements.END_DATE, day.endTime))
-            .then(() => this.click(WebElements.SAVE_ENTRY));
-        }
+      let promise = _.map(timeSheet, (days) => {
+        //this.checkDayStatus(days);
+        let innerPromise = _.map(days, (project) => {
+          if (project.totalHours > 0) {
+            return this.sendData(WebElements.SEARCH_WORK_ORDER, project.orderNumber)
+              .then(() => this.click(WebElements.SEARCH_BUTTON))
+              .then(() => this.click(WebElements.TYPE_HEAD))
+              .then(() => this.sendData(WebElements.START_DATE, project.startTime))
+              .then(() => this.sendData(WebElements.END_DATE, project.endTime))
+              .then(() => this.click(WebElements.SAVE_ENTRY));
+          }
+        });
+        return Promise.all(innerPromise);
       });
       Promise.all(promise).then(resolve, reject);
     })
@@ -88,8 +120,8 @@ class Execute {
     return this.driver.navigate().refresh().then(this.wait(1000))
   }
 
-  static dayBinder(day, span) {
-    return span ? By.xpath("//*[@id='" + day + "']//span[@class='DOWtime']") : By.xpath("//*[@id='" + day + "']//a");
+  static dayBinder(day, span, type) {
+    return span ? By.xpath(`//*[@id='${day}']//span[${type}]`) : By.xpath(`//*[@id='${day}']//a`);
   }
 
   getElementText(locator, timeOut = 10000) {
@@ -103,6 +135,20 @@ class Execute {
 
   wait(ms) {
     return this.driver.sleep(ms)
+  }
+
+  handleAlert() {
+    let alert = this.driver.switchTo().alert();
+    alert.getText().then((text) => {
+      console.log(`alert text: ${text}`);
+      alert.accept();
+    })
+  }
+
+  checkStatus(day) {
+    let query = [Execute.dayBinder(day, 'span', 2), Execute.dayBinder(day, 'span', 3), WebElements.STATUS];
+    let promise = _.map(query, (element) => this.getElementText(element));
+    return Promise.all(promise).then((text) => text).catch((error) => error);
   }
 }
 
--- server/time-sheet-parser.js
+++ server/time-sheet-parser.js
@@ -6,12 +6,14 @@ function parse(data) {
   let dates = timeSheet.dates;
   return _.reduce(dates, (result, d) => {
     result[d.date] = _.reduce(d.entries, (memo, e) => {
-      memo['projectName'] = e.name;
-      memo['orderNumber'] = e.ticketNumber;
-      memo['totalHours'] = e.total;
       let [startTime, endTime] = e.hours;
-      memo['startTime'] = startTime;
-      memo['endTime'] = endTime;
+      memo[e.name] = {
+        projectName: e.name,
+        orderNumber: e.ticketNumber,
+        totalHours: e.total,
+        startTime: startTime,
+        endTime: endTime
+      };
       return memo;
     }, {});
     return result;
